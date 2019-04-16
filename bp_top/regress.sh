704885f8eb298c150493ae1600734adc49eb0009
TavioGuarino <tavio.guarino@gmail.com>
2019-04-15 23:19:34 -0700

    Updated makefile for benchmarks. Included Dhrystone files. Close to working, believe that problem is in toolchain. Updated gnu toolchain configuration for our proper test enviorment since this is what github issue responses seem to be. going to run tool flow after.

--- bp_top/regress.sh
+++ bp_top/regress.sh
@@ -4,7 +4,7 @@
 i=`(ls $BP_TOP_DIR/test/rom/v/rv64*_rom.v | xargs -n 1 basename)`
 ISA_ROMS=`( for f in $i; do printf '%s\n' "${f%.v}" ; done )`
 
-BENCH_ROMS="median_rom multiply_rom towers_rom vvadd_rom"
+BENCH_ROMS="dhrystone_rom median_rom multiply_rom towers_rom vvadd_rom"
 
 echo "################# BP_TOP REGRESSION ###################"
 for ROM in $ISA_ROMS ; do
@@ -12,8 +12,6 @@ for ROM in $ISA_ROMS ; do
   make -C $BP_TOP_DIR/syn TEST_ROM=$ROM.v TRACE_ROM=$ROM.tr.v COVERAGE=VCS  bp_single_trace_demo.run.v \
     | grep "PASS" || echo "FAIL"
 done
-
-BENCH_ROMS="median_rom multiply_rom towers_rom vvadd_rom"
 
 echo "################# BP_TOP BENCH ###################"
 for ROM in $BENCH_ROMS ; do
